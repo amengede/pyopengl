7b4fad083e2217cce3d9ab36d392f92af9cc0f5e
amengede <amengede27@outlook.com>
2020-09-17 11:25:38 +1000

    200917

--- legacyTriangle.py
+++ legacyTriangle.py
@@ -2,9 +2,16 @@ import glfw
 from OpenGL.GL import *
 import numpy as np
 
+"""
+    create a rotating triangle
+"""
+
 if not glfw.init():
     raise Exception("glfw can not be initialised!")
 
+#glfw.create_window(w,h,title,monitor,share)
+#monitor: used for full screen, None if windowed
+#share: context with which to share resources, None if not sharing
 window = glfw.create_window(1280,720,"My OpenGL Window",None,None)
 
 if not window:
@@ -14,12 +21,21 @@ if not window:
 glfw.set_window_pos(window,400,200)
 glfw.make_context_current(window)
 
+#background color, (r,g,b,a)
 glClearColor(0,0.1,0.1,1)
 
+#x,y,z
+#
+#x: -1 = left
+#   1 = right
+#
+#y: -1 = up
+#   1 = down
 vertices = [-0.5,-0.5,0.0,
             0.5,-0.5,0.0,
             0.0,0.5,0.0]
 
+#r,g,b,a
 colors = [1.0,0.0,0.0,
             0.0,1.0,0.0,
             0.0,0.0,1.0]
